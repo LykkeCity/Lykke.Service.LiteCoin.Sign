14c97b7273ea81a3ea01e8f10b8b4e48c4ab15df
some-dev <some-dev@gmail.com>
2018-02-08 19:05:05 +0300

    add segwit support

--- src/Lykke.Service.LiteCoin.Sign.Services/Sign/TransactionSigningService.cs
+++ src/Lykke.Service.LiteCoin.Sign.Services/Sign/TransactionSigningService.cs
@@ -44,12 +44,12 @@ namespace Lykke.LiteCoin.Sign.Services.Sign
 
             var secretKeys = privateKeys.Select(p=>Key.Parse(p, _network)).ToList();
 
-            Key GetPrivateKey(KeyId pubKeyHash)
+            Key GetPrivateKey(TxDestination pubKeyHash)
             {
                 foreach (var secret in secretKeys)
                 {
                     var key = new BitcoinSecret(secret, _network);
-                    if (key.PubKey.Hash == pubKeyHash)
+                    if (key.PubKey.Hash == pubKeyHash || key.PubKey.WitHash == pubKeyHash || key.PubKey.WitHash.ScriptPubKey.Hash == pubKeyHash)
                         return key.PrivateKey;
                 }
 
@@ -83,8 +83,7 @@ namespace Lykke.LiteCoin.Sign.Services.Sign
 
                         continue;
                     }
-
-                    throw new BusinessException("Incompatible private key", ErrorCode.IncompatiblePrivateKey);
+                    
                 }
 
                 if (PayToPubkeyTemplate.Instance.CheckScriptPubKey(output.ScriptPubKey))
@@ -99,9 +98,38 @@ namespace Lykke.LiteCoin.Sign.Services.Sign
 
                         continue;
                     }
-
-                    throw new BusinessException("Incompatible private key", ErrorCode.IncompatiblePrivateKey);
                 }
+                if (PayToPubkeyTemplate.Instance.CheckScriptPubKey(output.ScriptPubKey))
+                {
+                    var secret = GetPrivateKey(PayToPubkeyTemplate.Instance.ExtractScriptPubKeyParameters(output.ScriptPubKey).Hash);
+                    if (secret != null)
+                    {
+                        var hash = Script.SignatureHash(output.ScriptPubKey, tx, i, hashType);
+                        var signature = secret.Sign(hash, hashType);
+
+                        tx.Inputs[i].ScriptSig = PayToPubkeyTemplate.Instance.GenerateScriptSig(signature);
+
+                        continue;
+                    }
+                    
+                }
+
+                if (PayToScriptHashTemplate.Instance.CheckScriptPubKey(output.ScriptPubKey) )
+                {
+                    var secret = GetPrivateKey(PayToScriptHashTemplate.Instance.ExtractScriptPubKeyParameters(output.ScriptPubKey));
+
+                    if (secret != null && secret.PubKey.WitHash.ScriptPubKey.Hash.ScriptPubKey == output.ScriptPubKey)
+                    {
+                        var hash = Script.SignatureHash(secret.PubKey.WitHash.AsKeyId().ScriptPubKey, tx, i, hashType, output.Value, HashVersion.Witness);
+                        var signature = secret.Sign(hash, hashType);
+                        tx.Inputs[i].WitScript = PayToPubkeyHashTemplate.Instance.GenerateScriptSig(signature, secret.PubKey);
+                        tx.Inputs[i].ScriptSig = new Script(Op.GetPushOp(secret.PubKey.WitHash.ScriptPubKey.ToBytes(true)));
+
+                        continue;
+                    }
+                    
+
+                };
 
 
                 throw new BusinessException("Incompatible private key", ErrorCode.InvalidScript);
