8aed1a77a93c469516f0332646c5bd47d5e79ff2
some-dev <some-dev@gmail.com>
2018-03-26 21:07:29 +0300

    remove InsightAPIUrl setting value

--- src/Lykke.Service.LiteCoin.Sign.Core/Settings/ServiceSettings/LiteCoinSignSettings.cs
+++ src/Lykke.Service.LiteCoin.Sign.Core/Settings/ServiceSettings/LiteCoinSignSettings.cs
@@ -3,9 +3,7 @@
     public class LiteCoinSignSettings
     {
         public DbSettings Db { get; set; }
-
-        // ReSharper disable once InconsistentNaming
-        public string InsightAPIUrl { get; set; }
+        
 
         public string Network { get; set; }
         
